ca48ca51f6086e8192c8a2958c6dbab6e585eb8b
zhangjh <wozhangjh@gmail.com>
2017-04-10 22:30:40 +0800

    add Person in models

--- audio/models.py
+++ audio/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+ 
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+class Testadd(models.Model):
+    address = models.CharField(max_length=30)
+    phone = models.IntegerField()
+
+    def __unicode__(self):
+        return self.address
