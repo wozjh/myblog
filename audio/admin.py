88b25daaf88a872bb82f2571d523967fcc050a82
zhangjh <wozhangjh@gmail.com>
2017-04-11 23:33:25 +0800

    django-admin

--- audio/admin.py
+++ audio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'update_time')
+
+admin.site.register(Article, ArticleAdmin)
--- audio/models.py
+++ audio/models.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
@@ -17,3 +18,14 @@ class Testadd(models.Model):
 
     def __unicode__(self):
         return self.address
+
+class Article(models.Model):
+    title = models.CharField(u'标题', max_length=30)
+    content = models.TextField(u'内容')
+
+    pub_date = models.DateTimeField(u'发布时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
