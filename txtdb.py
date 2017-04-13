7896432939648d81c4050ebf2f3b1214067ce0e4
zhangjh <wozhangjh@gmail.com>
2017-04-14 00:08:31 +0800

    Person.objects.create

---
+++ txtdb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*-
+
+import os
+# os.environ.setdefault('DJANGO_SETTINGS_MOUDLE', 'myblog.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
+import django
+if django.VERSION >= (1.7):
+    django.setup()
+
+def main():
+    from audio.models import Person
+    with open('oldblog.txt') as f:
+        for line in f:
+            name, age = line.split('****')
+            Person.objects.create(name=name, age=age)
+
+if __name__ == "__main__":
+    main()
+    print ('Done')
